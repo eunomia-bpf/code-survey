f0cf8f5723f550f9cf9668cd08599e48a50bd38e
littlefisher <i@littlefisher.me>
2024-09-10 06:41:34 +0000

    --allow-empty

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # test-git
+
+```
+git clone --mirror http://lore.kernel.org/bpf/0 bpf/git/0.git
+
+cd bpf/git/0.git
+
+chmod +x ../../../dump.sh
+../../../dump.sh
+
+# See emails at bpf/git/0.git/emails
+```
+
---
+++ dump.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create the emails directory if it doesn't exist
+mkdir -p emails
+
+# Get all the objects ending with 'm' from the git history
+git rev-list --all --objects | grep 'm$' | awk '{print $1}' | while read commit_id; do
+    # Output the content of the commit to a file named after the commit id in the emails/ directory
+    git cat-file -p $commit_id > "emails/${commit_id}.txt"
+done
