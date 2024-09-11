b62db15e71adcbf07a3cb9b61cc629bdb3dc9018
officeyutong <yt.xyxx@gmail.com>
2024-09-11 15:39:58 +0800

    update requirements and scripts

---
+++ .github/script/deploy_pygwalker.py
@@ -0,0 +1,24 @@
+from pygwalker.api.streamlit import StreamlitRenderer
+import pandas as pd
+import streamlit as st
+
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="Use Pygwalker In Streamlit",
+    layout="wide"
+)
+
+# Add Title
+st.title("Use Pygwalker In Streamlit")
+
+# You should cache your pygwalker renderer, if you don't want your memory to explode
+@st.cache_resource
+def get_pyg_renderer() -> "StreamlitRenderer":
+    df = pd.read_csv("../../data/feature_commit_details.csv")
+    # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
+    return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw")
+
+
+renderer = get_pyg_renderer()
+
+renderer.explorer()
